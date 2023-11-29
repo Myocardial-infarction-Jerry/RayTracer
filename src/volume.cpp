e450a1cba766c0de213dd8313d83b541505cf24e
Myocardial-infarction-Jerry <2428746252@qq.com>
2023-11-29 16:33:51 +0800

    rebuild from cuda kernel

--- src/volume.cpp
+++
@@ -1,5 +0,0 @@
-#include "volume.h"
-
-volume::volume(color(*_shader)(const vec3 &) = nullptr) { shader = _shader; }
-
-sphere::sphere(const point3 &_center, const double &_radius, color(*_shader)(const vec3 &) = nullptr) :center(_center), radius(_radius), volume(_shader) {}
