826adf3ab528d8b19a5898aedf48373649e84312
NTSCoS <2428746252@qq.com>
2024-02-28 17:05:27 +0800

    Re.

---
+++ src/CMakeFiles/RayTracer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../RayTracer"
+  "../RayTracer.pdb"
+  "CMakeFiles/RayTracer.dir/main.cpp.o"
+  "CMakeFiles/RayTracer.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
